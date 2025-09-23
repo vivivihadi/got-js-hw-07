b0b07de1d9e0762c50c37341524c39cddb2ce36b
vivivihadi <oliynykvika07@gmail.com>
2025-09-23 11:28:59 +0300

    task1

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,9 @@
+const categories = document.querySelector("#categories");
+
 console.log(`Number of categories: ${categories.querySelectorAll("li.item").length}`);
 
 categories.querySelectorAll("li.item").forEach(category => {
-  console.log('Category: ${category.querySelector("h2").textContent}');
-  console.log('Elements: ${category.querySelectorAll("li").length}');
+  console.log(`Category: ${category.querySelector("h2").textContent}`);
+  console.log(`Elements: ${category.querySelectorAll("li").length}`);
 
 });
-
